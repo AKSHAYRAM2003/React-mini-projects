7f439fa3995b51d9e524e4c06da6ebb2816363ed
Akshay  H R <ramakshay100@gmail.com>
2024-04-30 23:46:50 +0530

    environment setup

---
+++ props drill/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='width-full h-screen bg-zinc-500 font-["gilroy"] text-center p-5'>hello</h1>
+    </div>
+  )
+}
+
+export default App
